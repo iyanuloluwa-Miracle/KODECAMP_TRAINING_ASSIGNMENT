34759475a3476788afbf5a4231d55fb9a6fbfbda
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-07-17 14:27:42 +0100

    feat: Draw.io image for the school management added

--- KODECAMP_TASK_3/obj/Debug/net9.0/KODECAMP_TASK_3.AssemblyInfo.cs
+++ KODECAMP_TASK_3/obj/Debug/net9.0/KODECAMP_TASK_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KODECAMP_TASK_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cfddd7494ad08a85fbfd33feddac066841f81e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba4412ddd57eff7f0acbb0dcea2e352c5d0c613")]
 [assembly: System.Reflection.AssemblyProductAttribute("KODECAMP_TASK_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KODECAMP_TASK_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Going From Zero to Hero
+
+
