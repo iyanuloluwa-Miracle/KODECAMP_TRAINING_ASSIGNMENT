d495d8e8d6e6a11c9fe5495a7b6bf528580cb465
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-08-01 06:43:17 +0100

    chore: Update assembly information and regenerate binaries

--- KODECAMP_TASK5/obj/Debug/net9.0/KODECAMP_TASK5.AssemblyInfo.cs
+++ KODECAMP_TASK5/obj/Debug/net9.0/KODECAMP_TASK5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KODECAMP_TASK5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25712e2fac63af04b6e46e019aea2b985153d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a143ede0f35eb4fd25e73dfa355c15f1f2ec5c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("KODECAMP_TASK5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KODECAMP_TASK5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
